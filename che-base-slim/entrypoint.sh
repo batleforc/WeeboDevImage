77cff56498bbb5352713181316e23de50fcec385
Max Batleforc <maxleriche.60@gmail.com>
2026-06-14 00:51:36 +0200

    feat: wip

---
+++ che-base-slim/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Based on https://github.com/devfile/developer-images/blob/main/universal/ubi8/entrypoint.sh
+
+# Dynamically set subuid/subgid based on actual running UID for user namespace support
+USER_ID=$(id -u)
+START_ID=$(( USER_ID + 1 ))
+RANGE=$(( 65536 - START_ID ))
+echo "${USER:-user}:${START_ID}:${RANGE}" > /etc/subuid
+echo "${USER:-user}:${START_ID}:${RANGE}" > /etc/subgid
+
+# /home/user/ will be mounted to by a PVC if persistUserHome is enabled
+mountpoint -q /home/user/; HOME_USER_MOUNTED=$?
+
+# This file will be created after stowing, to guard from executing stow everytime the container is started
+STOW_COMPLETE=/home/user/.stow_completed
+
+if [ $HOME_USER_MOUNTED -eq 0 ] && [ ! -f $STOW_COMPLETE ]; then
+    # Create symbolic links from /home/tooling/ -> /home/user/
+    stow . -t /home/user/ -d /home/tooling/ --no-folding -v 2 > /tmp/stow.log 2>&1
+    # Vim does not permit .viminfo to be a symbolic link for security reasons, so manually copy it
+    cp /home/tooling/.viminfo /home/user/.viminfo
+    # We have to restore bash-related files back onto /home/user/ (since they will have been overwritten by the PVC)
+    # but we don't want them to be symbolic links (so that they persist on the PVC)
+    cp /home/tooling/.bashrc /home/user/.bashrc
+    cp /home/tooling/.bash_profile /home/user/.bash_profile
+    touch $STOW_COMPLETE
+fi
+
+exec "$@"
