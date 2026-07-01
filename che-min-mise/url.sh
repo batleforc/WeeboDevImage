9609984c70f91f36e38f4643d924120c570e9c03
Max Batleforc <maxleriche.60@gmail.com>
2026-07-01 18:11:25 +0000

    feat: wip
    Signed-off-by: Max Batleforc <maxleriche.60@gmail.com>

--- che-min-mise/url.sh
+++ che-min-mise/url.sh
@@ -4,7 +4,6 @@
 # Endpoint name defaults to "base" if omitted.
 set -euo pipefail
 
-ENDPOINT_NAME="${1:-base}"
 USERNAME="${DEVWORKSPACE_NAMESPACE##*-}"
 WORKSPACE="${DEVWORKSPACE_NAME:-}"
 DASHBOARD_URL="${CHE_DASHBOARD_URL:-}"
